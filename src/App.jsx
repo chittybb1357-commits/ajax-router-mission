b135dc17b5628cd2e611c3dfd6e3a5f99c75ddec
chittybb1357-commits <chittybb1357@gmail.com>
2026-06-26 16:25:33 +0900

    feat: update App component to include missing routes and import Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./components/Home";
+import Layout from "./components/Layout.jsx";
+import Header from "./components/Header.jsx";
+
+import Home from "./pages/Home.jsx";
+import Posts from "./pages/Posts.jsx";
+import PostDetail from "./pages/PostDetail.jsx";
+import PostNew from "./pages/PostNew.jsx";
+import PostEdit from "./pages/PostEdit.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    // blog.json 파일에서 데이터를 가져옵니다.
     fetch("./data/blog.json")
       .then(res => res.json())
       .then(data => {
         setPosts(data);
-        setLoaded(true); // 로드 완료 표시
+        setLoaded(true);
       })
       .catch(err => {
         console.error("데이터 로딩 실패:", err);
-        setLoaded(true); // 에러가 나도 로딩 창은 끝나도록 처리
+        setLoaded(true);
       });
   }, []);
 
@@ -30,10 +36,11 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout loaded={loaded} />}>
         <Route index element={<Home posts={posts} />} />
-        {/* 아래 주석 처리된 컴포넌트들도 구조에 맞게 구현되어 있어야 정상 동작합니다 */}
-        {/* <Route path="posts" element={<Posts posts={posts} />} /> */}
-        {/* <Route path="posts/:id" element={<PostDetail posts={posts} onDelete={handleDelete} />} /> */}
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="posts" element={<Posts posts={posts} />} />
+        <Route path="posts/new" element={<PostNew />} />
+        <Route path="posts/:id" element={<PostDetail posts={posts} onDelete={handleDelete} />} />
+        <Route path="posts/:id/edit" element={<PostEdit posts={posts} />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
