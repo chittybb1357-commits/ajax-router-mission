eb810d1bdd333a862841c421a99baceb18d44041
chittybb1357-commits <chittybb1357@gmail.com>
2026-06-29 14:36:04 +0900

    feat: add delete functionality and error handling in PostDetail component

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,11 +1,29 @@
-import { useParams, Link } from "react-router";
+import { useParams, Link, useNavigate } from "react-router";
 
-function PostDetail({ posts }) {
+function PostDetail({ posts, onDelete }) {
   const { id } = useParams();
+
+  let navigate = useNavigate();
 
   console.log(id);
 
   const post = posts.find(p => p.id === Number(id));
+
+  if (!post) {
+    return;
+    <>
+      <h2>에러</h2>
+      <p>존재하지 않는 게시물입니다.</p>
+      <Link to="/">홈으로 이동</Link>
+    </>;
+  }
+
+  const handleDelete = () => {
+    if (window.confirm("정말 삭제할까요?")) {
+      onDelete(post.id);
+      navigate("/dashboard");
+    }
+  };
 
   return (
     <>
@@ -18,7 +36,7 @@ function PostDetail({ posts }) {
       <div className="controls">
         <Link to={`post/edit/${post.id}`}>수정하기</Link>
 
-        <button>삭제하기</button>
+        <button onClick={handleDelete}>삭제하기</button>
       </div>
     </>
   );
