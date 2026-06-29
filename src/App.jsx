840ec8058fcfdf909ec926b92467d3d16a548bb6
chittybb1357-commits <chittybb1357@gmail.com>
2026-06-29 10:38:46 +0900

    refactor: clean up imports and improve fetch logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,41 @@
-import "./App.css";
 import { useState, useEffect } from "react";
+import "./App.css";
 import { Routes, Route } from "react-router";
-import Layout from "./components/Layout.jsx";
-
-import Home from "./pages/Home.jsx";
-import Posts from "./pages/Posts.jsx";
-import PostDetail from "./pages/PostDetail.jsx";
-import PostNew from "./pages/PostNew.jsx";
-import PostEdit from "./pages/PostEdit.jsx";
-import NotFound from "./pages/NotFound.jsx";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    let isAlive = true;
+    let alive = true; // 상품조회 시작..열일중...
 
-    fetch("./data/blog.json")
+    fetch("/data/blog.json")
       .then(res => res.json())
-      .then(data => {
-        if (isAlive) {
-          setPosts(data);
-          setLoaded(true);
-        }
-      })
-      .catch(err => {
-        console.error("데이터 로딩 실패:", err);
-
-        if (isAlive) {
-          setLoaded(true);
+      .then(result => {
+        if (alive) {
+          setPosts(result);
         }
       });
 
-    return () => {
-      isAlive = false;
-    };
+    return () => {}; //정리함수
   }, []);
 
-  const handleCreate = newPost => {
-    setPosts(prev => [newPost, ...prev]);
-  };
-  const handleUpdate = updatedPost => {
-    setPosts(prev => prev.map(post => (post.id === updatedPost.id ? updatedPost : post)));
-  };
-  const handleDelete = id => {
-    setPosts(prev => prev.filter(post => post.id !== id));
-  };
-
   return (
-    <Routes>
-      <Route element={<Layout loaded={loaded} />}>
-        <Route path="/" element={<Home posts={posts} />} />
-        <Route path="/posts" element={<Posts posts={posts} />} />
-        <Route path="/posts/new" element={<PostNew onCreate={handleCreate} />} />
-        <Route path="/posts/:id" element={<PostDetail posts={posts} onDelete={handleDelete} />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Layout loaded={loaded} />}>
+          <Route index element={<Home posts={posts} />} />
+          {/* <Route path="posts" element={<Posts posts={posts} />} />
         <Route
-          path="/posts/:id/edit"
-          element={<PostEdit posts={posts} onUpdate={handleUpdate} />}
+          path="posts/:id"
+          element={<PostDetail posts={} onDelete={}/>}
         />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Route path="*" element={<NotFound />}/> */}
+        </Route>
+      </Routes>
+    </>
   );
 }
 
